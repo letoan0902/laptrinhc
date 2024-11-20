b114dabe63ef95f9bd66b840618b473b0635a692
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-20 13:08:20 +0700

    Add files via upload

---
+++ Bai6session2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+	// khai bao
+	float pi=3.14;
+	float r = 3;
+	// chu vi
+	float perimeter = 2*pi*r;
+	// dien tich
+	float area = pi*r*r;
+	printf("%f  %f", perimeter, area);
+	return 0;
+}
