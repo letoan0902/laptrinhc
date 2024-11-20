88364fdf3b7d0c1e240a9a5d8b0048bfbea53973
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-20 13:07:03 +0700

    Add files via upload

---
+++ Bai1session2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	// kieu du lieu so nguyen
+	int a = 1;
+	// kieu du lieu so thuc
+	float pi = 3.14;
+	// kieu du lieu ky tu
+	char b = 'kytu';
+	// kieu du lieu so thuc voi do chinh xac cao hon
+	double c = 1.7777777777;
+	return 0;	 
+}
