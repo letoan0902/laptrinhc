64411a6e9644311c9882b8fa889ad2fea457dc96
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-20 13:07:49 +0700

    Add files via upload

---
+++ Bai3session2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	// khai bao
+	int a = 9, b = 3;
+	// tong
+	int c = a + b;
+	// hieu
+	int d = a - b;
+	// tich
+	int e = a * b;
+	// thuong
+	int f = a / b;
+	printf("Ket qua la %d , %d , %d , %d", c,d,e,f);
+	return 0;	 
+}
