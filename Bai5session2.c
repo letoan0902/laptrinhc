e317a76453e0d66829174e860b6d39bb5b6935d2
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-20 13:08:10 +0700

    Add files via upload

---
+++ Bai5session2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+	// khai bao canh hinh chu nhat
+	int rightSide = 5, leftSide = 6;
+	// chu vi
+	int perimeter = rightSide*2 + leftSide*2;
+	// dien tich
+	int area = rightSide*leftSide;
+	printf("%d  %d", perimeter, area);
+	return 0;
+	 
+} 
