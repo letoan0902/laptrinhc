7c4a17e8007c3ecc8e5761fb3556ec951e33007c
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-21 16:41:38 +0700

    Add files via upload

---
+++ Bai2session4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	printf("Hay nhap vao mot so nguyen ");
+	int n;
+	scanf("%d", &n);
+	if (n%2==0) {
+		printf("So nhap vao la so chan");
+		 
+	} else {
+		printf("So nhap vao la so le");
+	} 
+	return 0; 
+} 
