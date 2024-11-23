4c72b414d6ade3f42dce94a7bd577a03106ffff2
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-24 05:06:59 +0700

    Add files via upload

---
+++ Bai1session6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a[10]={0,1,2,3,4,5,6,7,8,9};
+	int i;
+	for (i=0;i<10;i++){
+		printf("%d ",a[i]);
+	}
+	printf("\nDo dai cua mang la 10");
+	
+	
+	return 0;
+}
