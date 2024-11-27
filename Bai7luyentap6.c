51f4c2945ab7cc2e2fef172accd505c390451430
letoan0902 <toanlp.b24dtcn385@stu.ptit.edu.vn>
2024-11-27 07:14:17 +0700

    Add files via upload

--- Bai7luyentap6.c
+++ Bai7luyentap6.c
@@ -1,23 +1,14 @@
 #include<stdio.h>
-    int main(){
-    int thang,nam;
-    printf("Nhap vao thang ");
-    scanf("%d",&thang);   
-    printf("Nhap vao nam ");
-    scanf("%d",&nam);
-	if (thang>12||thang<0){
-		printf("Du lieu nhap khong hop le");
-	} else if (thang==2){
-		if ((nam%4==0&&nam%100!=0) || (nam%400==0)){
-			printf("Thang co 29 ngay");
-		} else {
-			printf("Thang co 28 ngay");
+int main(){
+	int n,i;
+	printf("Hay nhap so: ");
+	scanf("%d",&n);
+	for (i=1;i<n;i++){
+		if (n % i == 0){
+			printf("\n%d",i);
 		}
-	} else if (thang==4||thang==6||thang==9||thang==11) {	
-		printf("Thang co 30 ngay");
 		
-	} else {
-		printf("Thang co 31 ngay");
 	}
-    return 0;	
-} 
+	
+	return 0;
+}
